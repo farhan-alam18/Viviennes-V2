e2bff5fd72eaddd4288df24bc46db49d331f3363
farhan-alam18 <farhanalam1113@gmail.com>
2023-10-29 18:13:58 +0530

    "Initialising using Vite "

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-slate-200 border-solid border-2 border-slate-950 p-5">
+      Hello world!
+    </h1>
+  )
+}
