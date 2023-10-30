f8f25bbfe557a0f6cbc9814a04a5d9b8b9cc6bfd
farhan-alam18 <farhanalam1113@gmail.com>
2023-10-30 11:20:15 +0530

    Routing & Imports Done

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import './index.css'
 
+// Page Imports 
+import Layout from "./Layout"
+import Home from './pages/Home/Home'
+import Shop from './pages/Shop/Shop'
+import Collections from './pages/Collections/Collections'
+import Contact from './pages/Contact/Contact'
+import Account from './pages/Account/Account'
+import Cart from './pages/Cart/Cart'
+
+// Creating Router -> Routing Paths 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
