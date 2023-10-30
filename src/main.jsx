279317bcc2ca1bd13302d5d5f6f32a95058e9961
farhan-alam18 <farhanalam1113@gmail.com>
2023-10-30 10:50:25 +0530

    Routing Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import './index.css'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path="" element={<Home/>} />
+      <Route path="shop" element={<Shop/>} />
+      <Route path="collections" element={<Collections/>} />
+      <Route path="about" element={<Contact/>} />
+      <Route path="account" element={<Account/>} />
+      <Route path="cart" element={<Cart/>} />
+    </Route>
+
+  )
+
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
